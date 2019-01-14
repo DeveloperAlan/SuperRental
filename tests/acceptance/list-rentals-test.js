7f492ffa63550917a1f407f803fca447bc49f329
Alan Ng <alan.ng@blake.com.au>
2019-01-15 10:10:07 +1100

    added acceptance and integration tests

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -30,4 +30,14 @@ module('Acceptance | list rentals', function (hooks) {
     await visit('/');
     assert.equal(this.element.querySelectorAll('.listing').length, 3, 'should display 3 listings');
   })
+
+  test('should filter the list of rentals by city.', function (assert) {
+      visit('/');
+      fillIn('.list-filter input', 'Seattle');
+      keyEvent('.list-filter input', 'keyup', 69);
+      andThen(function() {
+        assert.equal(find('.listing').length, 1, 'should show 1 listing');
+        assert.equal(find('.listing .location:contains("Seattle")').length, 1, 'should contain 1 listing with location Seattle');
+      });
+    });
 });
